7e42ea39aed8e2d37c434de8b147d65c29a92683
Nicolas Brassard <nico@nitriques.com>
2011-07-14 19:34:29 -0400

    added the js file for the publish pages

---
+++ assets/publish.oembed.js
@@ -0,0 +1,31 @@
+/*
+	Copyight: Solutions Nitriques 2011
+	License: MIT, see the LICENCE file
+*/
+
+/**
+ * Client code for managing the oEmbed Field on the
+ * publish page of sections
+ * 
+ * @author nicolasbrassard
+ */
+(function ($, undefined) {
+	
+	var FIELD = 'field-oembed',
+		FIELD_CLASS = '.' + FIELD;
+	
+	function switchToEdit() {
+		
+	};
+	
+	function hookOne() {
+		
+	};
+	
+	function init() {
+		$(FIELD_CLASS).each(hookOne);
+	};
+	
+	$(init);
+	
+})(jQuery);
