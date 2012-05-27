dfbfecf4566069a8cf1aecca4c43b2c0efc178de
Andrew Minton <me@andrewminton.co.uk>
2012-05-27 16:42:47 +0100

    Added permitted urls for JIT images into Flickr driver

--- lib/drivers/class.serviceFlickr.php
+++ lib/drivers/class.serviceFlickr.php
@@ -30,8 +30,27 @@
 
 		public function getNeededUrlsToJITimages() {
 			return array(
-				'http://www.flickr.com/*'
-				// @todo: complete
+				'http://www.flickr.com/*',
+				'http://farm1.static.flickr.com/*',
+				'http://farm2.static.flickr.com/*',
+				'http://farm3.static.flickr.com/*',
+				'http://farm4.static.flickr.com/*',
+				'http://farm5.static.flickr.com/*',
+				'http://farm6.static.flickr.com/*',
+				'http://farm7.static.flickr.com/*',
+				'http://farm8.static.flickr.com/*',
+				'http://farm9.static.flickr.com/*',
+				'http://farm10.static.flickr.com/*',
+				'http://farm1.staticflickr.com/*',
+				'http://farm2.staticflickr.com/*',
+				'http://farm3.staticflickr.com/*',
+				'http://farm4.staticflickr.com/*',
+				'http://farm5.staticflickr.com/*',
+				'http://farm6.staticflickr.com/*',
+				'http://farm7.staticflickr.com/*',
+				'http://farm8.staticflickr.com/*',
+				'http://farm9.staticflickr.com/*',
+				'http://farm10.staticflickr.com/*'
 			);
 		}
 	}
